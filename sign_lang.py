5ce688918b9746d15d8170549251c5e5137c13c3
Qi He <qihe.work@gmail.com>
2021-06-11 20:34:10 -0400

    debug

--- sign_lang.py
+++ sign_lang.py
@@ -76,13 +76,13 @@ class Sign():
             return False
         
     def letter_B(self):
-        pinky_mcp = self.xyz[17]
+        pinky_mcp = self.xyz[13]
         pinky_dip = self.xyz[19]
         ring_pip = self.xyz[14]
         thumb_tip = self.xyz[4]
         index_pip = self.xyz[6]
         middle_pip = self.xyz[10]
-        if distance(pinky_mcp, thumb_tip) < self.accuracy and distance(ring_pip, pinky_dip) < self.accuracy and distance(middle_pip , mring_pip) < self.accuracy:
+        if distance(pinky_mcp, thumb_tip) < self.accuracy and distance(ring_pip, pinky_dip) < self.accuracy and distance(middle_pip , index_pip) < self.accuracy:
             return True
         else:
             return False
